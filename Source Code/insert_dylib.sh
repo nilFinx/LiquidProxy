cd9e56cb382aa4e4bb34182d7ccc61a6b835710b
nilfinx <nulflox@disroot.org>
2026-01-20 15:32:13 +0900

    Remove insert_dylib, another Aqua Proxy stuff

--- Source Code/insert_dylib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /Users/Jonathan/Developer/AquaProxy/Package/Aqua\ Proxy/AquaProxy
-
-insert_dylib --inplace libWowfunhappyLegacySupport.dylib Aqua-HTTP-Proxy
-insert_dylib --inplace libMacportsLegacySupport.dylib Aqua-HTTP-Proxy
-insert_dylib --inplace libWowfunhappyLegacySupport.dylib Aqua-IMAP-Proxy
-insert_dylib --inplace libMacportsLegacySupport.dylib Aqua-IMAP-Proxy
